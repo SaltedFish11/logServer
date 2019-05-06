91ccaa5d7679526c6685e180ef9f0e55dc0976bf
yangwenrui <yangwenrui@shuidihuzhu.com>
2019-05-06 09:29:47 +0800

    fix

--- createLog.py
+++ createLog.py
@@ -4,9 +4,7 @@ import random
 import re
 import time
 import traceback
-import  sys
-reload(sys)
-sys.setdefaultencoding( "utf-8" )
+
 os_type = [
     '(Windows NT 6.1; WOW64)', '(Windows NT 10.0; WOW64)', '(X11; Linux x86_64)',
     '(Macintosh; Intel Mac OS X 10_12_6)', '(iPhone; CPU iPhone OS 12_1_2 like Mac OS X)',
@@ -16,12 +14,26 @@ os_type = [
 
 status_codes = ["200", "404", "500", "503", "403"]
 
+# 搜索引擎
+http_referers = [
+    "http://www.baidu.com/s?wd={query}",
+    "https://www.sougou.com/web?query={query}",
+    "http://cn.bing.com/search?q={query}",
+    "https://search.yahoo.com/search?q={query}"
+]
 
 def sample_url():
     url_paths = [
-        "https://www.jd.com/"
+        "class/112.html",
+        "class/128.html",
+        "class/145.html",
+        "class/146.html",
+        "class/131.html",
+        "class/130.html",
+        "learn/821",
+        "course/list"
     ]
-    return url_paths[0]
+    return random.sample(url_paths, 1)[0]
 
 # 判断ip是否合法
 def judge_legal_ip(one_str):
@@ -56,6 +68,14 @@ def sample_ip():
             return ip
     else:
         sample_ip()
+# 搜索的课程名字
+search_keyword = [
+    "Spark SQL实战",
+    "Hadoop基础",
+    "Storm实战",
+    "Spark Streaming实战",
+    "大数据面试"
+]
 
 # 请求时间
 def sample_time():
@@ -74,21 +94,14 @@ def sample_time():
 
 def sample_send_byte():
     return random.randint(0,5000)
-def sample_referer(cols):
+def sample_referer():
     if random.uniform(0, 1) > 0.2:
         return "-"
-    try:
-        refer_str = "https://www.jd.com/Search?keyword={query}"
-        key = str(cols[random.randint(0,len(cols)-1)]).lstrip()
-        split = key.split(' ')[0]
-        midd= split.encode('utf-8')
-        # query_str = str(key).decode('utf8')
-        # print query_str
-        res = refer_str.format(query=midd)
-        return res
-    except Exception, e:
-        msg = traceback.format_exc()
-        print(msg)
+
+    refer_str = random.sample(http_referers,1)
+    query_str = random.sample(search_keyword,1)
+    return refer_str[0].format(query = query_str[0])
+
 def sample_status_code():
     return random.sample(status_codes, 1)[0]
 
@@ -110,15 +123,15 @@ def sample_ua():
     return ua
 
 
-def generate_log(cols):
+def generate_log():
     try:
         f = open("/yang/access.log", "w+")
         count = 0
         while count<100:
-            referer = sample_referer(cols=cols)
-            query_log = '{ip} - - {date} "{method} {url} HTTP/1.1" {status_code} {send_byte} "{referer}" "{ua}"'.format(
+            query_log = '{ip} - - {date} "{method} {url} HTTP/1.1" {status_code} {send_byte} "{referer}" "{ua}"'.format\
+             (
                 ip=sample_ip(),  date=sample_time(), method=sample_method(), url=sample_url(), status_code=sample_status_code(),
-                send_byte=sample_send_byte(), referer=referer, ua=sample_ua())
+                send_byte=sample_send_byte(), referer=sample_referer(), ua=sample_ua())
 
             f.write(query_log + "\n")
             count = count + 1
@@ -126,36 +139,10 @@ def generate_log(cols):
     except Exception, e:
         msg = traceback.format_exc()
         print(msg)
-import xlrd
-
-def open_excel(file= 'file.xls'):
-    try:
-        xlrd.Book.encoding = "gbk"
-        data = xlrd.open_workbook(file)
-        return data
-    except Exception as e:
-        print(str(e))
-
-def excel_table_byname():#修改自己路径
-    print "读取文件开始"
-    file= './ywr.xlsx'
-    # by_name=u'Sheet1'
-    data = open_excel(file)
-    table = data.sheet_by_index(0) #获得表格
-    nrows = table.nrows  # 拿到总共行数
-    row = table.col_values(3)
-
-    list=[]
-    for rownum in range(0,nrows):
-
-        list.append(row[rownum])
-    print "读取文件结束"
-    return list
 
 if __name__ =="__main__":
     try:
-        cols = excel_table_byname()
-        generate_log(cols)
+        generate_log()
     except Exception, e:
         msg = traceback.format_exc()
         print(msg)
--- test.py
+++
@@ -1,5 +0,0 @@
-
-if __name__=="__main__":
-    refer_str = "无师自通说韩语（2VCD+2CD-ROM+MP3音频文件+2教材） "
-    l = refer_str.split("/s")
-    print l[0]
